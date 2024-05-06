dc669cebf3800cd1e2f43ba771478ba5b9084a28
abounsir <hamidbounsir@gmail.com>
2024-05-06 11:07:11 +0100

    export Timer

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Timer from './src/components/timer';
-
-function App(): React.JSX.Element {
-  return <Timer time={500} play={true} />;
-}
-
-export default App;
